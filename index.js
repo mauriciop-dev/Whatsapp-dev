67263f647e3ef454eb06b1028139899d114b69ec
Mauricio Pineda <142756854+mauriciop-dev@users.noreply.github.com>
2026-07-04 12:47:39 -0500

    Agrega vercel.json para Express serverless + module.exports

--- index.js
+++ index.js
@@ -208,3 +208,5 @@ async function enviarDocumentoWhatsApp(to, phoneId, pdfUrl) {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+
+module.exports = app;
